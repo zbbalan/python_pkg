79d180e102a9f4c75bd35979f77444094e61801c
ZBB <test@ZBB.mshome.net>
2023-08-14 14:40:05 +0800

    增加test（练习）目录,

---
+++ test/求1-100奇数和.py
@@ -0,0 +1,10 @@
+i = 1
+num = 0
+while i <= 100 :
+    if i%2 == 0:
+        i+1
+    else:
+        num = num + i
+       
+    i +=1
+    print(num)
---
+++ variable/while_循环嵌套.py
@@ -0,0 +1,9 @@
+###while循环嵌套
+num = 1
+while num <= 3:
+    print('第一层循环------------------')
+    Num1 = 1
+    while Num1 <= 4:
+        print('第二层循环')
+        Num1 +=1
+    num +=1
